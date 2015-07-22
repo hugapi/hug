418be2bfa0f902183b607fa402da75c09bf7e6db
Timothy Crosley <timothy.crosley@gmail.com>
2015-07-22 01:04:04 -0700

    Use falcon methods as HTTP methods

--- hug/decorators.py
+++ hug/decorators.py
@@ -2,13 +2,15 @@ from functools import wraps
 from collections import OrderedDict
 import sys
 
+from falcon import HTTP_METHODS
 
-def call(url, methods=('ALL', )):
+
+def call(url, methods=HTTP_METHODS):
     def decorator(api_function):
         module = sys.modules[api_function.__name__]
-        api_definition = sys.modules['hug.hug'].__dict__.setdefault('API_CALLS', OrderedDict())
+        api_definition = sys.modules['hug.hug'].__dict__.setdefault('HUG_API_CALLS', OrderedDict())
         for method in methods:
-            api_definition.setdefault(url, {})['ALL'] = api_function
+            api_definition.setdefault(url, {})["on_{0}".format(method.lower())] = api_function
 
         def interface(request, reponse):
             return api_function(**request.attributes)
@@ -16,6 +18,7 @@ def call(url, methods=('ALL', )):
         api_function.interface = interface
 
         return api_function
+
 
 def get(url):
     return call(url=url, accept=('GET', ))
