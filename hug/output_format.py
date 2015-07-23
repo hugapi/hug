9ae94436dee20e6daa66418e032d520117bd588a
Timothy Crosley <timothy.crosley@gmail.com>
2015-07-23 09:24:09 -0700

    add camelcase helper

--- hug/output_format.py
+++ hug/output_format.py
@@ -7,3 +7,18 @@ def json(content):
 
 def text(content):
     return content.encode('utf8')
+
+
+def _camelcase(dictionary):
+    if not isinstance(dictionary, dict):
+        return dictionary
+
+    new_dictionary = {}
+    for key, value in dictionary.items():
+        if isinstance(key, str):
+            key = key[0] + "".join(key.title().split('_'))[1:]
+        new_dictionary[key] = _camelcase(dictionary)
+
+
+def json_camelcase(content):
+    return _camelcase(json(body))
