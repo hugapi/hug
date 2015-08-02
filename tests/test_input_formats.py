e33882df05dbc710a39c96d376e84dd114a4cbf1
Timothy Crosley <timothy.crosley@gmail.com>
2015-08-02 14:27:00 -0700

    Add test for hug input_formats

---
+++ tests/test_input_formats.py
@@ -0,0 +1,32 @@
+"""tests/test_input_formats.py.
+
+Tests the input format handlers included with hug
+
+Copyright (C) 2015 Timothy Edmund Crosley
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+import hug
+
+
+def test_json():
+    test_data = '{"a": "b"}'
+    assert hug.input_format.json(test_data) == {'a': 'b'}
+
+
+def test_json_underscore():
+    test_data = '{"CamelCase": {"becauseWeCan": "ValueExempt"}}'
+    assert hug.input_format.json_underscore(test_data) == {'camel_case': {'because_we_can': 'ValueExempt'}}
