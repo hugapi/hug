ad3aebe17ce3ac3ca5bc0836f4e1622b1526f6e0
Timothy Crosley <timothy.crosley@gmail.com>
2015-08-08 04:45:44 -0700

    Add support for overriding output_type globably and per module

--- hug/decorators.py
+++ hug/decorators.py
@@ -5,28 +5,54 @@ from functools import partial
 
 from falcon import HTTP_BAD_REQUEST, HTTP_METHODS
 
-import hug.output_format
 from hug.run import server
+import hug.defaults
+import hug.output_format
 
 
 class HugAPI(object):
     '''Stores the information necessary to expose API calls within this module externally'''
-    __slots__ = ('versions', 'routes')
+    __slots__ = ('versions', 'routes', '_output_format')
 
-    def __init__(self, versions=()):
-        self.versions = set(versions)
+    def __init__(self):
+        self.versions = set()
         self.routes = OrderedDict()
 
+    @property
+    def output_format(self):
+        return getattr(self, '_output_format', hug.defaults.output_format)
 
-def call(urls=None, accept=HTTP_METHODS, output=hug.output_format.json, examples=(), versions=None, stream_body=False):
+    @output_format.setter
+    def output_format(self, formatter):
+        self._output_format = formatter
+
+
+def default_output_format(content_type='application/json'):
+    """A decorator that allows you to override the default output format for an API"""
+    def decorator(formatter):
+        module = sys.modules[formatter.__module__]
+        formatter = hug.output_format.content_type(content_type)(formatter)
+        module.__hug__.output_format = formatter
+        return formatter
+    return decorator
+
+
+def call(urls=None, accept=HTTP_METHODS, output=None, examples=(), versions=None, stream_body=False):
     urls = (urls, ) if isinstance(urls, str) else urls
     examples = (examples, ) if isinstance(examples, str) else examples
     versions = (versions, ) if isinstance(versions, (int, float, None.__class__)) else versions
 
     def decorator(api_function):
         module = sys.modules[api_function.__module__]
+        if not '__hug__' in module.__dict__:
+            def api_auto_instantiate(*kargs, **kwargs):
+                module.__hug_wsgi__ = server(module)
+                return module.__hug_wsgi__(*kargs, **kwargs)
+            module.__hug__ = HugAPI()
+            module.__hug_wsgi__ = api_auto_instantiate
         accepted_parameters = api_function.__code__.co_varnames[:api_function.__code__.co_argcount]
         takes_kwargs = bool(api_function.__code__.co_flags & 0x08)
+        function_output = output or module.__hug__.output_format
 
         defaults = {}
         for index, default in enumerate(reversed(api_function.__defaults__ or ())):
@@ -37,7 +63,7 @@ def call(urls=None, accept=HTTP_METHODS, output=hug.output_format.json, examples
             use_examples = (True, )
 
         def interface(request, response, **kwargs):
-            response.content_type = output.content_type
+            response.content_type = function_output.content_type
             input_parameters = kwargs
             input_parameters.update(request.params)
             if request.content_type == "application/json" and not stream_body:
@@ -59,22 +85,16 @@ def call(urls=None, accept=HTTP_METHODS, output=hug.output_format.json, examples
                 if not require in input_parameters:
                     errors[require] = "Required parameter not supplied"
             if errors:
-                response.data = output({"errors": errors})
+                response.data = function_output({"errors": errors})
                 response.status = HTTP_BAD_REQUEST
                 return
 
             if not takes_kwargs:
                 input_parameters = {key: value for key, value in input_parameters.items() if key in accepted_parameters}
 
-            response.data = output(api_function(**input_parameters))
+            response.data = function_output(api_function(**input_parameters))
 
-        if not '__hug__' in module.__dict__:
-            def api_auto_instantiate(*kargs, **kwargs):
-                module.__hug_wsgi__ = server(module)
-                return module.__hug_wsgi__(*kargs, **kwargs)
-            module.__hug__ = HugAPI(versions)
-            module.__hug_wsgi__ = api_auto_instantiate
-        elif versions:
+        if versions:
             module.__hug__.versions.update(versions)
 
         for url in urls or ("/{0}".format(api_function.__name__), ):
@@ -86,14 +106,15 @@ def call(urls=None, accept=HTTP_METHODS, output=hug.output_format.json, examples
 
         api_function.interface = interface
         interface.api_function = api_function
-        interface.output_format = output
+        interface.output_format = function_output
         interface.examples = use_examples
         interface.defaults = defaults
         interface.accepted_parameters = accepted_parameters
-        interface.content_type = output.content_type
+        interface.content_type = function_output.content_type
         return api_function
     return decorator
 
 
 for method in HTTP_METHODS:
     globals()[method.lower()] = partial(call, accept=(method, ))
+
