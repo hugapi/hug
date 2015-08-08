b6cc066b63f7a97d6f03763f7d9e3dc0d0acf241
Timothy Crosley <timothy.crosley@gmail.com>
2015-08-08 01:53:42 -0700

    Auto jsonify input body if its not a string

--- hug/test.py
+++ hug/test.py
@@ -3,12 +3,15 @@ from falcon import HTTP_METHODS
 from urllib.parse import urlencode
 import json
 from hug.run import server
+from hug import output_format
 from functools import partial
 
 
 def call(method, api_module, url, body='', headers=None, **params):
     api = server(api_module)
     response = StartResponseMock()
+    body = output_format.json(body) if not isinstance(body, str) else body
+
     result = api(create_environ(path=url, method=method, headers=headers, query_string=urlencode(params), body=body),
                  response)
     if result:
@@ -16,6 +19,7 @@ def call(method, api_module, url, body='', headers=None, **params):
         response.content_type = response.headers_dict['content-type']
         if response.content_type == 'application/json':
             response.data = json.loads(response.data)
+
     return response
 
 
