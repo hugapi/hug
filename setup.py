b6f112e7bf73329b92dfd4147db240fcc8d403a8
Timothy Crosley <timothy.crosley@gmail.com>
2015-10-13 00:41:53 -0700

    Bump version to 1.6.0

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ except (IOError, ImportError, OSError, RuntimeError):
    readme = ''
 
 setup(name='hug',
-      version='1.5.1',
+      version='1.6.0',
       description='A Python framework that makes developing APIs as simple as possible, but no simpler.',
       long_description=readme,
       author='Timothy Crosley',
