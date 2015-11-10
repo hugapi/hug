dc1118525e47854d8185d4d46db5a12ec3e93aea
Timothy Crosley <timothy.crosley@gmail.com>
2015-11-10 13:48:58 -0800

    Add example post body processer

---
+++ examples/post_body_example.py
@@ -0,0 +1,17 @@
+'''A simple post reading server example.
+
+To test run this server with `hug -f post_body_example`
+then run the following from ipython:
+    import requests
+
+    requests.post('http://localhost:8000.post_here', json={'one': 'two'}).json()
+
+    This should return back the json data that you posted
+'''
+import hug
+
+
+@hug.post()
+def post_here(body):
+    '''This example shows how to read in post data w/ hug outside of its automatic param parsing'''
+    return body
