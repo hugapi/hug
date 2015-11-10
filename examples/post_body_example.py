234124b55abfb03c9e809f35ea746988031938a2
Timothy Crosley <timothy.crosley@gmail.com>
2015-11-10 14:21:38 -0800

    Fix syntax error

--- examples/post_body_example.py
+++ examples/post_body_example.py
@@ -4,7 +4,7 @@ To test run this server with `hug -f post_body_example`
 then run the following from ipython:
     import requests
 
-    requests.post('http://localhost:8000.post_here', json={'one': 'two'}).json()
+    requests.post('http://localhost:8000/post_here', json={'one': 'two'}).json()
 
     This should return back the json data that you posted
 '''
