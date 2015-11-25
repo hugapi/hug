b894baf115603487d4fd8d8134413673d0178d4c
Timothy Crosley <timothy.crosley@gmail.com>
2015-11-25 12:16:31 -0800

    Fix hello_world example

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,15 +1,9 @@
-from falcon import HTTP_206
 import hug
 
 
-def change_status(content, response):
-    response.status = HTTP_206
-    return content
-
-
-@hug.get(transform=change_status)
+@hug.get()
 def hello():
     '''Says hello'''
-    raise ValueError('hiii')
+    return 'Hello World!'
 
 
