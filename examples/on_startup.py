29f7dcbe4f3469d051d92344963821a4199644cb
Timothy Crosley <timothy.crosley@gmail.com>
2016-01-13 23:38:28 -0800

    Add an example of the intended implementation of the  feature

---
+++ examples/on_startup.py
@@ -0,0 +1,22 @@
+"""Provides an example of attaching an action on hug server startup"""
+import hug
+
+data = []
+
+
+@hug.startup()
+def add_data(api):
+    '''Adds initial data to the api on startup'''
+    data.append("It's working")
+
+
+@hug.startup()
+def add_more_data(api):
+    '''Adds initial data to the api on startup'''
+    data.append("Even subsequent calls")
+
+
+@hug.get()
+def test():
+    '''Returns all stored data'''
+    return data
