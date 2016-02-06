a6a78260b47f3a632564e7a80ce25b3b75e242e9
Ian Wagner <ianthetechie@gmail.com>
2016-02-06 00:48:51 -0500

    Add sample code for API key authentication
    Sample code demonstrates the use of `X-Api-Key` authentication,
    implementation of a trivial authentication function, and using a rich
    user type rather than a string.

--- examples/authentication.py
+++ examples/authentication.py
@@ -10,16 +10,41 @@ import hug
 # illustrative purposes.
 authentication = hug.authentication.basic(hug.authentication.verify('User1', 'mypassword'))
 
+@hug.get('/public')
+def public_api_call():
+    return "Needs no authentication"
 
 # Note that the logged in user can be accessed via a built-in directive.
 # Directives can provide computed input parameters via an abstraction
 # layer so as not to clutter your API functions with access to the raw
 # request object.
 @hug.get('/authenticated', requires=authentication)
-def api_call1(user: hug.directives.user):
-    return "Successfully authenticated with user: {0}".format(user)
+def basic_auth_api_call(user: hug.directives.user):
+    return 'Successfully authenticated with user: {0}'.format(user)
 
 
-@hug.get('/public')
-def api_call2():
-    return "Needs no authentication"
+# Here is a slightly less trivial example of how authentication might
+# look in an API that uses keys.
+
+# First, the user object stored in the context need not be a string,
+# but can be any Python object.
+class APIUser(object):
+    """A minimal example of a rich User object"""
+    def __init__(self, user_id, api_key):
+        self.user_id = user_id
+        self.api_key = api_key
+
+def api_key_verify(api_key):
+    magic_key = '5F00832B-DE24-4CAF-9638-C10D1C642C6C' # Obviously, this would hit your database
+    if api_key == magic_key:
+        # Success!
+        return APIUser('user_foo', api_key)
+    else:
+        # Invalid key
+        return None
+
+api_key_authentication = hug.authentication.api_key(api_key_verify)
+
+@hug.get('/key_authenticated', requires=api_key_authentication)
+def basic_auth_api_call(user: hug.directives.user):
+    return 'Successfully authenticated with user: {0}'.format(user.user_id)
