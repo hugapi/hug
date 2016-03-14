f82b146a14c7ba2f75604216922539284327e2b8
Fabian Kochem <fkochem@gmail.com>
2016-03-14 21:55:25 +0100

    Enforce PEP8, abort build if issues are found

--- examples/write_once.py
+++ examples/write_once.py
@@ -6,7 +6,7 @@ import requests
 @hug.local()
 @hug.cli()
 @hug.get()
-def top_post(section: hug.types.one_of(('news', 'newest', 'show'))='news'):
+def top_post(section:hug.types.one_of(('news', 'newest', 'show'))='news'):
     """Returns the top post from the provided section"""
     content = requests.get('https://news.ycombinator.com/{0}'.format(section)).content
     text = content.decode('utf-8')
