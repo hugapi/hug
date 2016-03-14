44129bdcaf6a2c5dcd2feb5f4e166a11af51fa2c
Timothy Crosley <timothy.crosley@gmail.com>
2016-03-14 00:47:00 -0700

    Add examples from quick start

---
+++ examples/quick_start/first_step_2.py
@@ -0,0 +1,10 @@
+"""First hug API (local and HTTP access)"""
+import hug
+
+
+@hug.get(examples='name=Timothy&age=26')
+@hug.local()
+def happy_birthday(name:hug.types.text, age:hug.types.number, hug_timer=3):
+    """Says happy birthday to a user"""
+    return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
+            'took': float(hug_timer)}
