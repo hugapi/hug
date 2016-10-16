128f151b2f03c6fc3f7b3d5d4693a92b7057d4db
Timothy Crosley <timothy.crosley@gmail.com>
2016-10-15 19:56:02 -0700

    Fix tox configuration; switch back to original CLI example

--- examples/cli.py
+++ examples/cli.py
@@ -1,9 +1,12 @@
 """A basic cli client written with hug"""
 import hug
 
-@hug.cli()
-def cli(*values):
-    return values
+
+@hug.cli(version="1.0.0")
+def cli(name: 'The name', age: hug.types.number):
+    """Says happy birthday to a user"""
+    return "Happy {age} Birthday {name}!\n".format(**locals())
+
 
 if __name__ == '__main__':
     cli.interface.cli()
