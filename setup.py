98422db6b4716230e7ff91f04417c12c4580751c
Timothy Crosley <timothy.crosley@gmail.com>
2017-03-27 23:34:27 -0700

    Add explicit support for Python 3.6

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setup(name='hug',
                    'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
                    'Topic :: Software Development :: Libraries',
                    'Topic :: Utilities'],
       **PyTest.extra_kwargs)
