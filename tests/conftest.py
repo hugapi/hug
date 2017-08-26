bf424bb858c696ccefb635c82b31a5a8b4d442dd
Timothy Crosley <timothy.crosley@gmail.com>
2017-08-25 20:44:35 -0700

    Seperate out typing specific tests so they only run on supported versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ collect_ignore = []
 
 if sys.version_info < (3, 5):
     collect_ignore.append("test_async.py")
+    collect_ignore.append("test_typing.py")
 
 if sys.version_info < (3, 4):
     collect_ignore.append("test_coroutines.py")
