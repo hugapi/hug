bc8e77b214480fe37c5292591a8e408cdc3449a5
Timothy Crosley <timothy.crosley@gmail.com>
2017-09-19 22:43:32 -0700

    Fix unicode example syntax

--- examples/unicode_output.py
+++ examples/unicode_output.py
@@ -1,6 +1,7 @@
 """A simple example that illustrates returning UTF-8 encoded data within a JSON outputting hug endpoint"""
 import hug
 
+
 @hug.get()
 def unicode_response():
     """An example endpoint that returns unicode data nested within the result object"""
