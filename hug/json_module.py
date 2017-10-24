73c230730b251a834f408f2d90ba6cb5f5448506
Dani Gonzalez <dani@robbie.ai>
2017-10-24 08:13:47 -0400

    - Fix unsupported `default` kwarg when using ujson - Mimic json built-in behavior by settings `escape_forward_slashes=False`

--- hug/json_module.py
+++ hug/json_module.py
@@ -4,6 +4,18 @@ HUG_USE_UJSON = bool(os.environ.get('HUG_USE_UJSON', 1))
 try:
     if HUG_USE_UJSON:
         import ujson as json
+
+        class dumps_proxy:
+            """Proxies the call so non supported kwargs are skipped
+            and it enables escape_forward_slashes to simulate built-in json
+            """
+            _dumps = json.dumps
+
+            def __call__(self, *args, **kwargs):
+                kwargs.pop('default', None)
+                kwargs.update(escape_forward_slashes=False)
+                return self._dumps(*args, **kwargs)
+        json = dumps_proxy()
     else:
         import json
 except ImportError:
