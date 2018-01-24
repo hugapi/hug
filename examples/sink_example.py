ad1f7d4d562bd891239c053a39c04f3a8ed00192
Timothy Crosley <timothy.crosley@gmail.com>
2018-01-24 11:56:05 -0800

    Fit 120 line length limt

--- examples/sink_example.py
+++ examples/sink_example.py
@@ -1,6 +1,7 @@
 """This is an example of a hug "sink", these enable all request URLs that start with the one defined to be captured
 
-To try this out, run this api with hug -f sink_example.py and hit any URL after localhost:8000/all/ (for example: localhost:8000/all/the/things/) and it will return the path sans the base URL. 
+To try this out, run this api with hug -f sink_example.py and hit any URL after localhost:8000/all/
+(for example: localhost:8000/all/the/things/) and it will return the path sans the base URL. 
 """
 import hug
 
