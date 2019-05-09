3080c44c23adcb3a09fb94343da872b8b26ce9fc
Timothy Crosley <timothy.crosley@gmail.com>
2019-05-09 00:51:50 -0700

    Remove no longer necessary test ignore logic

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,3 @@
 import sys
 
 from .fixtures import *
-
-collect_ignore = []
-
-if sys.version_info < (3, 5):
-    collect_ignore.append("test_async.py")
-
-if sys.version_info < (3, 4):
-    collect_ignore.append("test_coroutines.py")
